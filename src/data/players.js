322b8754b98ad5b511162b4a2e7da9735917edf9
Argo Roots <argo.roots@adm.ee>
2022-05-20 14:58:26 +0300

    Move properties and players to separate files

---
+++ src/data/players.js
@@ -0,0 +1,51 @@
+import HatIcon from '@/assets/icons/icons8-top-hat-60.png'
+import BattleshipIcon from '@/assets/icons/icons8-battleship-60.png'
+import CarIcon from '@/assets/icons/icons8-sedan-60.png'
+import DogIcon from '@/assets/icons/icons8-shiba-inu-60.png'
+import CatIcon from '@/assets/icons/icons8-pet-commands-stay-60.png'
+import TrexIcon from '@/assets/icons/icons8-dinosaur-60.png'
+import PenguinIcon from '@/assets/icons/icons8-penguin-60.png'
+import DuckyIcon from '@/assets/icons/icons8-rubber-ducky-60.png'
+
+export const players = [
+  {
+    name: 'Top Hat',
+    icon: HatIcon,
+    money: 1500
+  },
+  {
+    name: 'Battleship',
+    icon: BattleshipIcon,
+    money: 1500
+  },
+  {
+    name: 'Race Car',
+    icon: CarIcon,
+    money: 1500
+  },
+  {
+    name: 'Scottie Dog',
+    icon: DogIcon,
+    money: 1500
+  },
+  {
+    name: 'Cat (Hazel)',
+    icon: CatIcon,
+    money: 1500
+  },
+  {
+    name: 'T-Rex',
+    icon: TrexIcon,
+    money: 1500
+  },
+  {
+    name: 'Penguin',
+    icon: PenguinIcon,
+    money: 1500
+  },
+  {
+    name: 'Rubber Ducky',
+    icon: DuckyIcon,
+    money: 1500
+  }
+]
