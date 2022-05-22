41f34dc5405e02138a225e8c0e7b981ea5e30f34
Argo Roots <argo.roots@adm.ee>
2022-05-22 17:46:58 +0300

    Fix player list scroll

--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -25,30 +25,32 @@ const selectedId = computed({
 </script>
 
 <template>
-  <div class="m-10 overflow-y-auto flex flex-col justify-center text-stone-900/80 divide-y divide-stone-900/20">
-    <div
-      v-for="(p, idx) in players"
-      :key="idx"
-      class="py-2 px-1 flex flex-row justify-between items-center hover:bg-emerald-50 cursor-pointer"
-      :class="{
-        'bg-emerald-50': selectedId === idx
-      }"
-      @click="selectedId = idx"
-    >
-      <img
-        :src="p.icon"
-        class="h-4 w-4"
-      >
-      <div class="grow ml-2">
-        {{ p.name }}
-      </div>
+  <div class="m-10 overflow-y-auto ">
+    <div class="flex flex-col justify-center text-stone-900/80 divide-y divide-stone-900/20">
       <div
-        class="font-mono"
+        v-for="(p, idx) in players"
+        :key="idx"
+        class="py-2 px-1 flex flex-row justify-between items-center hover:bg-emerald-50 cursor-pointer"
         :class="{
-          'text-red-700': p.balance < 0
+          'bg-emerald-50': selectedId === idx
         }"
+        @click="selectedId = idx"
       >
-        £{{ p.balance }}
+        <img
+          :src="p.icon"
+          class="h-4 w-4"
+        >
+        <div class="grow ml-2">
+          {{ p.name }}
+        </div>
+        <div
+          class="font-mono"
+          :class="{
+            'text-red-700': p.balance < 0
+          }"
+        >
+          £{{ p.balance }}
+        </div>
       </div>
     </div>
   </div>
