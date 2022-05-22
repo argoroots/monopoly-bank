c0cc33c6b47d7e6d88fa1233d2a31835387b4893
Argo Roots <argo.roots@adm.ee>
2022-05-22 17:01:02 +0300

    Add more players

--- src/data/players.js
+++ src/data/players.js
@@ -1,51 +1,39 @@
-import HatIcon from '@/assets/icons/icons8-top-hat-60.png'
+import BagIcon from '@/assets/icons/icons8-bag-60.png'
 import BattleshipIcon from '@/assets/icons/icons8-battleship-60.png'
-import CarIcon from '@/assets/icons/icons8-sedan-60.png'
-import DogIcon from '@/assets/icons/icons8-shiba-inu-60.png'
-import CatIcon from '@/assets/icons/icons8-pet-commands-stay-60.png'
-import TrexIcon from '@/assets/icons/icons8-dinosaur-60.png'
+import BootIcon from '@/assets/icons/icons8-footwear-60.png'
+import CatHazelIcon from '@/assets/icons/icons8-pet-commands-stay-60.png'
+import DogScottieIcon from '@/assets/icons/icons8-shiba-inu-60.png'
+import HatIcon from '@/assets/icons/icons8-top-hat-60.png'
+import HorseRiderIcon from '@/assets/icons/icons8-horseback-riding-60.png'
+import HowitzerIcon from '@/assets/icons/icons8-cannon-60.png'
+import IronIcon from '@/assets/icons/icons8-ironing-60.png'
+import LanternIcon from '@/assets/icons/icons8-lantern-60.png'
+import MoneyBagIcon from '@/assets/icons/icons8-money-bag-pounds-60.png'
 import PenguinIcon from '@/assets/icons/icons8-penguin-60.png'
-import DuckyIcon from '@/assets/icons/icons8-rubber-ducky-60.png'
+import RaceCarIcon from '@/assets/icons/icons8-sedan-60.png'
+import RockingHorseIcon from '@/assets/icons/icons8-rocking-horse-60.png'
+import RubberDuckyIcon from '@/assets/icons/icons8-rubber-ducky-60.png'
+import ThimbleIcon from '@/assets/icons/icons8-thimble-60.png'
+import TrexIcon from '@/assets/icons/icons8-dinosaur-60.png'
+import WheelbarrowIcon from '@/assets/icons/icons8-wheelbarrow-60.png'
 
 export const allPlayers = [
-  {
-    name: 'Top Hat',
-    icon: HatIcon,
-    balance: 1500
-  },
-  {
-    name: 'Battleship',
-    icon: BattleshipIcon,
-    balance: 1500
-  },
-  {
-    name: 'Race Car',
-    icon: CarIcon,
-    balance: 1500
-  },
-  {
-    name: 'Scottie Dog',
-    icon: DogIcon,
-    balance: 1500
-  },
-  {
-    name: 'Cat (Hazel)',
-    icon: CatIcon,
-    balance: 1500
-  },
-  {
-    name: 'T-Rex',
-    icon: TrexIcon,
-    balance: 1500
-  },
-  {
-    name: 'Penguin',
-    icon: PenguinIcon,
-    balance: 1500
-  },
-  {
-    name: 'Rubber Ducky',
-    icon: DuckyIcon,
-    balance: 1500
-  }
+  { name: 'Bag', icon: BagIcon, balance: 1500 },
+  { name: 'Battleship', icon: BattleshipIcon, balance: 1500 },
+  { name: 'Boot', icon: BootIcon, balance: 1500 },
+  { name: 'Cat Hazel', icon: CatHazelIcon, balance: 1500 },
+  { name: 'Dog Scottie', icon: DogScottieIcon, balance: 1500 },
+  { name: 'Horse & Rider', icon: HorseRiderIcon, balance: 1500 },
+  { name: 'Howitzer', icon: HowitzerIcon, balance: 1500 },
+  { name: 'Iron', icon: IronIcon, balance: 1500 },
+  { name: 'Lantern', icon: LanternIcon, balance: 1500 },
+  { name: 'Money Bag', icon: MoneyBagIcon, balance: 1500 },
+  { name: 'Penguin', icon: PenguinIcon, balance: 1500 },
+  { name: 'Race Car', icon: RaceCarIcon, balance: 1500 },
+  { name: 'Rocking Horse', icon: RockingHorseIcon, balance: 1500 },
+  { name: 'Rubber Ducky', icon: RubberDuckyIcon, balance: 1500 },
+  { name: 'T-Rex', icon: TrexIcon, balance: 1500 },
+  { name: 'Thimble', icon: ThimbleIcon, balance: 1500 },
+  { name: 'Top Hat', icon: HatIcon, balance: 1500 },
+  { name: 'Wheelbarrow', icon: WheelbarrowIcon, balance: 1500 }
 ]
